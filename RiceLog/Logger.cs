f7856faf9ea596ee3f23fa596783f9ee0d800448
ricepuffz <28723269+ricepuffz@users.noreply.github.com>
2021-07-14 18:18:02 +0200

    Add project files.

---
+++ RiceLog/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RiceLog
+{
+    public class Logger
+    {
+        public static VERBOSITY CurrentVerbosity = VERBOSITY.ERROR;
+
+        private Logger() { }
+
+        public enum VERBOSITY
+        {
+            NONE = 0,
+            ERROR = 1,
+            WARN = 2,
+            INFO = 3
+        }
+
+        public static void Log(String message, VERBOSITY verbosity)
+        {
+            if (verbosity <= CurrentVerbosity)
+            {
+                string verbosityName = Enum.GetName(typeof(VERBOSITY), verbosity);
+                Console.WriteLine($"[{verbosityName}] {message}");
+            }
+        }
+
+        public static void Log(String message)
+        {
+            Log(message, VERBOSITY.INFO);
+        }
+    }
+}
